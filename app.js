77374ca56555b2c27a21b649e9ff0fc30f614ddb
Colin Edwards <ColinJhEdwards@gmail.com>
2022-05-24 19:42:13 -0700

    initial set up

---
+++ app.js
@@ -0,0 +1,6 @@
+// being able to create tests is extremely beneficial due to the ability to automatically test the features
+// present on a website. Instead of manually testing the functionality of our website everytime we change a line
+// of code we can instead create tests that will do it for us automatically. Saving a large amount of time.
+
+// Unit tests focus on testing individual units(usually functons), while integration tests make sure everything
+// works together
