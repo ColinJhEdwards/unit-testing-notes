dc6e5e13c45c9861dc71c8424664525d930616f3
Colin Edwards <ColinJhEdwards@gmail.com>
2022-06-01 19:27:18 -0700

    updated notes

--- app.js
+++ app.js
@@ -3,4 +3,11 @@
 // of code we can instead create tests that will do it for us automatically. Saving a large amount of time.
 
 // Unit tests focus on testing individual units(usually functons), while integration tests make sure everything
-// works together
+// works together. End to end testing is replicating a user actually using the application.
+
+// Tools needed for testing
+// - A test runner(jest/karma): runs your tests(i.e the testing code you write, that is not apart of your main project code)
+// - Assertion Library(jest/chai): Used to define expected outcomes in tests and will check if said expectations are met
+
+// Vitest is new testing technology that is becoming quite popular amongst the community, especially because of how easily it works with
+// module js (import/export), something jest can have issues with. The code can vitest can also be written the same way jest is.
